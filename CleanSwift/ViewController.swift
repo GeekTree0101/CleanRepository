00a3ae95edcf99019796b9c6966d9fb6b862b190
Geektree0101 <h2s1880@gmail.com>
2019-08-30 08:59:42 +0900

    Initial Commit

---
+++ CleanSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwift
+//
+//  Created by Hyeon su Ha on 30/08/2019.
+//  Copyright © 2019 Hyeon su Ha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
